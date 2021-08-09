377e337ce92c299e73d6923e5301f3cecb83471c
Gustavo <lima.gustavo100@hotmail.com>
2021-08-09 19:52:19 -0300

    Concluido sem documentação

--- Scripts/hroads-DML.sql
+++ Scripts/hroads-DML.sql
@@ -1,2 +1,39 @@
 USE HROADS;
 GO
+
+INSERT INTO CLASSE (nomeClasse)
+VALUES	('Bárbaro'),('Cruzado'),('Caçadora de Demônios'),
+		('Monge'),('Necromante'),('Feiticeiro'),('Arcanista')
+GO
+
+INSERT INTO TIPOHABILIDADE (nomeTipoHabilidade)
+VALUES	('Ataque'),('Defesa'),('Cura'),('Magia')
+GO
+
+INSERT INTO PERSONAGEM (idClasse, nomePersonagem, capacidadeMaxVida,
+capacidadeMaxMana, dataAtualizacao, dataCriacao)
+VALUES	(1,'DeuBug',100,80,GETDATE(),'18/01/2019 17:35:21'),
+		(4,'BitBug',70,100,GETDATE(),'17/03/2016 09:45:03'),
+		(7,'Fer8',75,60,GETDATE(),'18/03/2018 16:02:01')
+GO
+
+INSERT INTO HABILIDADE (idTipoHabilidade, nomeHabilidade)
+VALUES	(1,'Lança Mortal'),
+		(2,'Escudo Supremo'),
+		(3,'Recuperar Vida')
+GO
+
+INSERT INTO CLASSEHABILIDADE (idClasse, idHabilidade)
+VALUES	(1,1),(1,2),(2,2),(3,1),(4,3),(4,2),(6,3)
+GO
+
+UPDATE PERSONAGEM
+SET nomePersonagem = 'Fer7'
+WHERE nomePersonagem = 'Fer8'
+GO
+
+UPDATE CLASSE 
+SET nomeClasse = 'Necromancer'
+WHERE nomeClasse = 'Necromante'
+GO
+
--- Scripts/hroads-DQL.sql
+++ Scripts/hroads-DQL.sql
@@ -1,2 +1,61 @@
 USE HROADS;
 GO
+
+SELECT * FROM PERSONAGEM
+GO
+
+SELECT * FROM CLASSE
+GO
+
+SELECT nomeClasse FROM CLASSE
+GO
+
+SELECT * FROM HABILIDADE
+GO
+
+SELECT COUNT(*) FROM HABILIDADE
+GO
+
+SELECT idHabilidade FROM HABILIDADE
+ORDER BY idHabilidade ASC
+GO
+
+SELECT * FROM TIPOHABILIDADE
+GO
+
+SELECT nomeHabilidade Habilidade, nomeTipoHabilidade [Tipo Habilidade] FROM HABILIDADE H
+LEFT JOIN TIPOHABILIDADE TH
+ON H.idTipoHabilidade = TH.idTipoHabilidade
+GO
+
+SELECT nomePersonagem Personagem, nomeClasse Classe FROM PERSONAGEM P
+LEFT JOIN CLASSE C
+ON P.idClasse = C.idClasse
+GO
+
+SELECT nomePersonagem Personagem, nomeClasse Classe FROM PERSONAGEM P
+RIGHT JOIN CLASSE C
+ON P.idClasse = C.idClasse
+GO
+
+SELECT nomeClasse Classe, nomeHabilidade Habilidade FROM CLASSE C
+LEFT JOIN CLASSEHABILIDADE CH
+ON C.idClasse = CH.idClasse
+LEFT JOIN HABILIDADE H
+ON CH.idHabilidade = H.idHabilidade
+GO
+
+SELECT nomeHabilidade Habilidade, nomeClasse Classe FROM CLASSE C
+LEFT JOIN CLASSEHABILIDADE CH
+ON C.idClasse = CH.idClasse
+INNER JOIN HABILIDADE H
+ON CH.idHabilidade = H.idHabilidade
+GO
+
+SELECT nomeHabilidade Habilidade, nomeClasse Classe FROM CLASSE C
+FULL OUTER JOIN CLASSEHABILIDADE CH
+ON C.idClasse = CH.idClasse
+FULL OUTER JOIN HABILIDADE H
+ON CH.idHabilidade = H.idHabilidade
+GO
+
